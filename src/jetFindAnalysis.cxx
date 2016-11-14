4e2ce77839cc168aea85676ae530aadc9697ae7c
nick-elsey <nicholas.elsey@gmail.com>
2016-11-14 12:38:38 -0500

    testing

--- src/jetFindAnalysis.cxx
+++ src/jetFindAnalysis.cxx
@@ -157,7 +157,7 @@ int main( int argc, const char** argv ) {
   
   // create the pythia generator and initialize it with the xmldoc in my pythia8 directory
   //Pythia8::Pythia pythia( xmldir );
-  Pythia8::Pythia pythia();
+  Pythia8::Pythia pythia;
   
   // settings for LHC pp at 13 TeV
   pythia.readString("Beams:eCM = 13000");
