0db7a5968526e4b5eecebaba9dc7e3411f975f15
nick-elsey <nicholas.elsey@gmail.com>
2016-11-14 12:40:20 -0500

    testing

--- src/jetFindAnalysis.cxx
+++ src/jetFindAnalysis.cxx
@@ -160,7 +160,7 @@ int main( int argc, const char** argv ) {
   Pythia8::Pythia pythia;
   
   // settings for LHC pp at 13 TeV
-  pythia.readString("Beams:eCM = 13000");
+  //pythia.readString("Beams:eCM = 13000");
   pythia.readString("HardQCD:all = on");
   //pythia.readString("Random:setSeed = on");
   //pythia.readString("Random:seed = 0");
